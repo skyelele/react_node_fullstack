cc8632172172d028ad5d47420d73a56b98d6214f
Skye Leahy <skyeleahy@protonmail.com>
2019-03-10 23:52:14 -0700

    add code for user deletion, user post relationship with poast schema, and image upload

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,25 +1,46 @@
 const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
 
 // exports.getPosts = (req, res) => {
 //     res.send("Hey world from node JAY ESS");
 // }
 
 exports.getPosts = (req, res) => {
-    res.json({
-        posts: [{ title: "First Post"}, {title: "Second Post"}]
-    });
+    const posts = Post.find()
+        .select("_id title body")
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(err => console.log(err));
 };
 
-exports.createPost = (req, res) => {
-    const post = new Post(req.body);
-    post.save((err, result) => {
-        if (err) {
+exports.createPost = (req, res, next) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) {
             return res.status(400).json({
-                error: err
+                error: "Image could not be uploaded"
             });
         }
-        res.status(200).json({
-            post:result
+        let post = new Post(fields);
+        
+        req.profile.hashed_password = undefined;
+        req.profile.salt = undefined;
+        post.postedBy = req.profile;
+
+        if(files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+        post.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(result);
         });
     });
 };
